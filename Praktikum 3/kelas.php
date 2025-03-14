6ccad5d09f0a79c12b8cd33b98d94f93853304d8
Karim-Rimuru <mekar.10817@gmail.com>
2025-03-14 09:04:43 +0700

    Tambah

--- Praktikum 3/kelas.php
+++ Praktikum 3/kelas.php
@@ -1,7 +1,37 @@
 <?php
 class Mahasiswa {
-    public $nama;
-    public $nim;
-    public $semester;
+    private $nama;
+    private $nim;
+    private $semester;
+
+    public function setNama ($nama) {
+        $this->nama = $nama;
+    }
+    public function setNim ($nim) {
+        $this->nim = $nim;
+    }
+    public function setSemester ($semester) {
+        $this->semester = $semester;
+    }
+    public function getNama(){
+        return $this->nama;
+    }
+    public function getNim () {
+        return $this->nim;
+    }
+
+    public function getSemester () {
+        return $this->semester;
+    }
+
 }
+
+$Budi = new Mahasiswa();
+$Budi->setNama('budi santoso');
+$Budi->setNim('0110217043');
+$Budi->setSemester(3);
+
+echo $Budi->getNama().'<br>';
+echo $Budi->getNim().'<br>';
+echo $Budi->getSemester().'<br>';
 ?>
